78bff813c3510e193ce78464a1e505226ad0f531
NotPiny <103984634+NotPiny@users.noreply.github.com>
2024-04-07 17:12:39 +0100

    Create site or smth

---
+++ src/routes/Games/+page.svelte
@@ -0,0 +1,11 @@
+<h1>
+    Games
+</h1>
+<p>
+    Literally just a list of games. I'm not sure what you expected.
+</p>
+<ul>
+    <li>
+        <a href="/Games/rickrolldle">Rickrolldle</a>
+    </li>
+</ul>
